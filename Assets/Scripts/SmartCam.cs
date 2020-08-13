0988358f5715a10eb0670de4bfb241dfb5b8e16e
tobeefansis <40244173+tobeefansis@users.noreply.github.com>
2020-08-13 13:25:46 +0300

    all

---
+++ Assets/Scripts/SmartCam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SmartCam : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+    new Transform transform;
+    [Range(1, 20)] [SerializeField] float Speed = 1;
+
+    private void Awake()
+    {
+        transform = base.transform;
+    }
+    // Start is called before the first frame update
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * Speed) + new Vector3(0, 0, -10);
+
+    }
+}
