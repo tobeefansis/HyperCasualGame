f26f4db9305cf9d7653075ae968819270ba92af2
tobeefansis <40244173+tobeefansis@users.noreply.github.com>
2020-08-14 13:52:52 +0300

    Player Settings

--- Assets/Scripts/PlayerSettings.cs
+++ Assets/Scripts/PlayerSettings.cs
@@ -1,7 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+[CreateAssetMenu()]
 public class PlayerSettings : SavedObject
 {
+    public int BestCount;
 
+    public void Set(int count)
+    {
+        if (count > BestCount)
+        {
+            BestCount = count;
+        }
+    }
 }
