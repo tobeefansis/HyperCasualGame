9ed755de42b1e62d0d1f06db74add4d829f18265
tobeefansis <40244173+tobeefansis@users.noreply.github.com>
2020-08-14 13:53:50 +0300

    Update PlayerStandPlatform.cs

--- Assets/Scripts/PlayerStandPlatform.cs
+++ Assets/Scripts/PlayerStandPlatform.cs
@@ -15,7 +15,13 @@ public class PlayerStandPlatform : MonoBehaviour
     {
 
     }
-
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
